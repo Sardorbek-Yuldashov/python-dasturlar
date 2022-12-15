d2651d212bc956022d9d0663ff45562232f5a8f1
Sardorbek-Yuldashov <118550473+Sardorbek-Yuldashov@users.noreply.github.com>
2022-12-15 20:16:17 +0300

    Add files via upload

---
+++ 1-15.py
@@ -0,0 +1,98 @@
+# 1-15 mashqlar 
+"""
+Created on Fri Dec  9 19:52:58 2022
+
+@author: Yuldashov Sardor
+"""
+
+# x=input().split()
+# x=int(x)
+# v=x**3
+# print("%.2f"%v)
+
+# Radiuslari r1, r2, r3 bo’lgan 3 to doira radiuslari berilgan. Doiralarni yuzlarini hisoblang.
+# from math import pi
+# r1,r2,r3=input().split()
+# r1,r2,r3=int(r1),int(r2),int(r3)
+# s1=pi*r1**2
+# s2=pi*r2**2
+# s3=pi*r3**2
+# print("%.2f %.2f %.2f" % (s1 ,s2 , s3))
+
+# Yuzasi s va balandligi h bo`lgan uch burchak berilgan. Uni asosini toping.
+#a=(2s)/h
+# s,h=input().split()
+# s,h=int(s),int(h)
+# a=float(2*s)/h
+# print("asos ","%.2f"%float( a), "ga teng")
+
+
+# Radiusi r bo`lgan sharning yuzini toping.
+# Kiruvchi ma’lumotlar: bitta butun son   
+# Chiquvchi ma’lumotlar: bitta son masala yechimi. Javobni  aniqlikda chiqaring.
+# from math import pi
+# R=input()
+# R=int(R)
+# s=4*pi*R**2
+# print("%.2f"%s)
+
+
+# a,b va c tamonli uch burchak berilgan. Uchburchakning yarim perimetri topilsin.
+
+# a,b,c=input().split()
+# a,b,c=int(a),int(b),int(c)
+# p=(a+b+c)/2
+# print("%.2f"%p)
+
+# # Konusni balandligi h va radiusi r bo`lsa uni hajmi nimaga teng bo`ladi.
+# from math import pi
+# h,r=input().split()
+# h,r=int(h),int(r)
+# v=(1/3)*pi*r**2*h
+# print("%.2f"%v)
+# # # Tezligi v bo`lgan avtomabil s masofani qancha vaqtda bosib o`tadi.
+# v,s=input().split()
+# v,s=int(v),int(s)
+# t=s/v
+# print("%.2f" % t)
+
+# h balandlikqan erkin tushayotgan jism qancha vaqtdan keyin yerga uriladi(g=9.8)
+# from math import sqrt
+# h=int(input())
+# g=9.8
+# t=sqrt((2*h)/g)
+# print("%.2f"%t)
+
+# # Jo`mrakdan 1 s da 1 milli litr suv tomsa x yilda necha litr suv tomadi.
+# x=int(input())
+# yil_x=x*369*24*60*60*1
+# print(yil_x)
+# 1 dan n gacha sonlar berilgan. Berilgan sonlarni yig`indisini toping.
+# n=int(input())
+# son=(1+n/2)*n
+# print(son)
+
+# Massasi m bo`lgan jismni og`irligini toping(g=9.8)
+# m=input()
+# m=int(m)
+# g=9.8
+# p=m*g
+# print("%.2f"%p)
+
+# m massali jismga a tezlanish berilganda unga qanchali kuch tasir qiladi.
+# m,a=input().split()
+# m,a=int(m),int(a)
+# f=m*a
+# print(f)
+
+# Zanjiring kuchlanishi  va qarshiligi . Zanjirdan oqib o`tayotgan to`q kuchini toping.
+# u,r=input().split()
+# u,r=int(u),int(r)
+# i=u/r
+# print("%.3f"%i)
+
+# Zanjirlaring R1 ,R2  va R3  qarshilig o`zaro parallel ulangan. Ularnig umumiy qarshiligini toping.
+# r1,r2,r3=input().split()
+# r1,r2,r3=int(r1),int(r2),int(r3)
+# _1_r=1/r1+1/r2+1/r3
+# print("%.2f"%_1_r)
